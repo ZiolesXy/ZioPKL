f88616fc64aa48484a0fe92468b2a1d460a3dc5d
ZiolesXy <160699873+ZiolesXy@users.noreply.github.com>
2026-04-22 08:52:22 +0700

    fix: fix not clear data in minio

--- Voca Hub/server/scripts/teardown.ps1
+++ Voca Hub/server/scripts/teardown.ps1
@@ -2,6 +2,6 @@ Write-Host "Stopping containers..." -ForegroundColor Yellow
 
 docker rm -f redis-dev 2>$null
 docker rm -f minio-dev 2>$null
-docker volume prune -a -f
+docker volume prune -f
 
 Write-Host "Stopped." -ForegroundColor Green
